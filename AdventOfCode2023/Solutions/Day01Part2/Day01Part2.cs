df9e8e1526e68d9a4cbe6563594ba34c75ed286b
a21liltr <a21liltr@student.his.se>
2023-12-02 09:31:19 +0100

    Finish Day 1 Part 1. Starting Part 2.

---
+++ AdventOfCode2023/Solutions/Day01Part2/Day01Part2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Solutions.Day01Part2
+{
+    class Day01Part2
+    {
+        public int value { get; set; }
+        public string filePath { get; set; }
+        public List<string> numbers { get; set; }
+        public Day01Part2(string filepath)
+        {
+            this.filePath = filepath;
+            this.numbers = new List<string>();
+            numbers.Add("zero");
+            numbers.Add("one");
+            numbers.Add("two");
+            numbers.Add("three");
+            numbers.Add("four");
+            numbers.Add("five");
+            numbers.Add("six");
+            numbers.Add("seven");
+            numbers.Add("eight");
+            numbers.Add("nine");
+        }
+
+        public int Solution(List<string> lines)
+        {
+            StreamReader reader = new StreamReader(filePath);
+            string line = reader.ReadLine();
+
+            while (line != null)
+            {
+                lines.Add(line);
+                line = reader.ReadLine();
+            }
+
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            dictionary.Add("one", 1);
+            dictionary.Add("two", 2);
+            dictionary.Add("three", 3);
+            dictionary.Add("four", 4);
+            dictionary.Add("five", 5);
+            dictionary.Add("six", 6);
+            dictionary.Add("seven", 7);
+            dictionary.Add("eight", 8);
+            dictionary.Add("nine", 9);
+
+
+            foreach(string text in lines)
+            {
+
+            }
+
+
+            foreach (string text in lines)
+            {
+                string digits = "";
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (char.IsNumber(text[i]))
+                    {
+                        digits += text[i];
+                        break;
+                    }
+                }
+
+                for (int i = text.Length - 1; i >= 0; i--)
+                {
+                    if (char.IsNumber(text[i]))
+                    {
+                        digits += text[i];
+                        Console.WriteLine("The number is: " + digits);
+                        value += int.Parse(digits);
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("My output: " + value);
+            this.value = value;
+            return value;
+        }
+
+        string GetFirst(string text)
+        {
+            Match match = Regex.Matches()
+        }
+
+    }
+}
---
+++ AdventOfCode2023/Solutions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2023.Solutions;
+using AdventOfCode2023.Solutions.Day01Part1;
+
+namespace AdventOfCode2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory = @"C:\Users\tranl\_programming\AdventOfCode\AdventOfCode2023\Inputs\";
+            string fileName = "Day01Part1.txt";
+            string path = Path.Combine(directory, fileName);
+            List<string> lines = new();
+
+            
+
+           
+        }
+    }
+}
